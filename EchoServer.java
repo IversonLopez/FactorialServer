ef3ee947e43c6766abed13755b54c16c0989dc00
IversonLopez <iverson9lopez@gmail.com>
2024-09-29 15:17:59 -0400

    first

---
+++ EchoServer.java
@@ -0,0 +1,28 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class EchoServer{
+    public static void main(String[] args){
+
+        //using try and catch methods when dealing with IO'S
+
+        try{
+            System.out.println("Waiting for clients");
+            ServerSocket ss = new ServerSocket(9086);
+            Socket soc = ss.accept();//blocking call, the server socet will wait for a incoming cilient connections and then when 
+                                     //it gets a conncetion it will return a socket object, and that object will capture it in a 
+                                     //Socket reference.
+            System.out.println("Connection Established");  // if the previous method from the ServerSocket class(.accept) works, then say Connection Established
+
+        }
+        
+
+        catch(Exception e){
+
+            e.printStackTrace();
+
+        }
+
+    }
+}
